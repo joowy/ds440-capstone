09bf342178034b50515fee8fe250e0fca2d2e5d2
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-10-30 17:46:43 -0400

    fetch localhost

--- client/source/Popup/Popup.jsx
+++ client/source/Popup/Popup.jsx
@@ -1,22 +1,9 @@
 import * as React from "react";
-import browser from "webextension-polyfill";
 
 import "./styles.scss";
 
-import { PythonShell } from "python-shell";
-
 function runPythonCode() {
-  //   return browser.tabs.create({ url });
-
-  //   PythonShell.runString("x=1+1;print(x)", null, function (err) {
-  //     if (err) throw err;
-  //     console.log("finished");
-  //   });
-
-  PythonShell.run("../../test.py", null, function (err) {
-    if (err) throw err;
-    console.log("finished");
-  });
+  fetch("http://localhost:8000/");
 }
 
 const Popup = () => {
