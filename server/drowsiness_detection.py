72314c87b0169ffb6789a3d0031fa61120cd19f2
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-11-13 15:32:29 -0500

    press space

--- server/drowsiness_detection.py
+++ server/drowsiness_detection.py
@@ -1,12 +1,17 @@
-from scipy.spatial import distance
-from imutils.video import VideoStream
-from imutils import face_utils
-import numpy as np
 import argparse
-import imutils
 import time
-import dlib
+
 import cv2
+import dlib
+import imutils
+import numpy as np
+import win32api
+from imutils import face_utils
+from imutils.video import VideoStream
+from scipy.spatial import distance
+from win32con import KEYEVENTF_EXTENDEDKEY, VK_MEDIA_PLAY_PAUSE
+from pynput.keyboard import Key, Controller
+import pynput
 
 
 def eye_aspect_ratio(eye):
@@ -17,16 +22,18 @@ def eye_aspect_ratio(eye):
     return EAR
 
 
-threshold_value = 0.25
-frames = 20
+keyboard = Controller()
+
+threshold_value = 0.22
+frames = 30
+
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor(
-    "./shape_predictor_68_face_landmarks.dat")
+# load facial landmark model
+predict = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# start the video stream thread
 cap = cv2.VideoCapture(0)
 flag = 0
 while True:
@@ -36,7 +43,7 @@ while True:
     objects = detect(gray, 0)
     for x in objects:
         shape = predict(gray, x)
-        shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
+        shape = face_utils.shape_to_np(shape)
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
@@ -50,10 +57,12 @@ while True:
             flag += 1
             print(flag)
             if flag >= frames:
-                cv2.putText(frame, "****************ALERT!****************",
-                            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.putText(frame, "****************ALERT!****************",
-                            (10, 325), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                keyboard.press(Key.space)
+                # keyboard.press("a")
+
+                cv2.destroyAllWindows()
+                cap.release()
+                break
         else:
             flag = 0
     cv2.imshow("Frame", frame)
