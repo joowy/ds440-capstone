289dd6467e1dc71bbeb88022824a56539ae4169d
Joseph Stout <46057052+joowy@users.noreply.github.com>
2022-11-28 22:10:57 -0500

    get tabs

--- client/source/Background/index.js
+++ client/source/Background/index.js
@@ -1,6 +1,46 @@
-import 'emoji-log';
-import browser from 'webextension-polyfill';
+import "emoji-log";
+import browser from "webextension-polyfill";
 
 browser.runtime.onInstalled.addListener(() => {
-  console.emoji('🦄', 'extension installed');
+  console.emoji("🦄", "extension installed");
 });
+
+let currentTab;
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    // currentTab = tab.url;
+    activateCV(tab.url);
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+  if (tab.active && change.url) {
+    // currentTab = tab.url;
+    activateCV(tab.url);
+  }
+});
+
+const activateCV = (currentTab) => {
+  console.log(currentTab);
+  if (currentTab == "") {
+    fetch("http://localhost:8000/");
+  }
+};
+
+// class main {
+//   constructor() {}
+
+//   init = async () => {
+
+//     console.log("current tab?" + currentTab);
+//     if (currentTab == "https://www.youtube.com/") {
+//       console.log("er");
+//     } else {
+//       console.log("asd");
+//     }
+//   };
+// }
+
+// const app = new main();
+// app.init().catch(() => {});
